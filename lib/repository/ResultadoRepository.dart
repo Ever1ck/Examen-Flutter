a7615b937540bffc58bd99e09b176334963e6895
Ever1ck <64207368+Ever1ck@users.noreply.github.com>
2022-12-05 12:21:45 -0500

    Hola

---
+++ lib/repository/ResultadoRepository.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:app_upeu/apis/resultado_api.dart';
+import 'package:app_upeu/local/dao/ResultadoDao.dart';
+import 'package:app_upeu/modelo/ResultadoModelo.dart';
+import 'package:app_upeu/util/NetworConnection.dart';
+import 'package:dio/dio.dart';
+
+class ResultadoRepository {
+  ResultadoApi resultadoApi;
+  ResultadoLocate resultadoLocal;
+  bool result;
+
+  ResultadoRepository() {
+    Dio _dio = Dio();
+    _dio.options.headers["Content-Type"] = "application/json";
+    resultadoApi = ResultadoApi(_dio);
+    resultadoLocal = ResultadoLocate();
+  }
+
+  Future<List<ResultadoModelo>> getResultado() async {
+    return await resultadoLocal.getAllResultado();
+  }
+
+  Future<ResponseModelo> deleteResultado(int id) async {
+    return await resultadoApi.deleteResultado(id);
+  }
+
+  Future<ResponseModelo> updateResultado(
+      int id, ResultadoModelo resultado) async {
+    return await resultadoApi.updateResultado(id, resultado);
+  }
+
+  Future<ResponseModelo> createResultado(ResultadoModelo resultado) async {
+    return await resultadoApi.createResultado(resultado);
+
+    resultadoLocal.insertResultado(resultado);
+    return ResponseModelo();
+  }
+}
